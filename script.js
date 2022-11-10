bec023664b5b0de742ce9a2c7e6849c03d37b2cf
Carl Hammarling <carlhammarling@gmail.com>
2022-11-11 00:03:17 +0100

    måttomvandalre

---
+++ script.js
@@ -0,0 +1,102 @@
+const form = document.querySelector('#form');
+
+// let portion4 = document.querySelector('#portion4').value
+
+const matSpan1 = document.querySelector('#matSpan1')
+const matSpan2 = document.querySelector('#matSpan2')
+const matSpan3 = document.querySelector('#matSpan3')
+const matSpan4 = document.querySelector('#matSpan4')
+const matSpan5 = document.querySelector('#matSpan5')
+const matSpan6 = document.querySelector('#matSpan6')
+const matSpan7 = document.querySelector('#matSpan7')
+const matSpan8 = document.querySelector('#matSpan8')
+const matSpan9 = document.querySelector('#matSpan9')
+const matSpan10 = document.querySelector('#matSpan10')
+const matSpan11 = document.querySelector('#matSpan11')
+const matSpan12 = document.querySelector('#matSpan12')
+const matSpan13 = document.querySelector('#matSpan13')
+const matSpan14 = document.querySelector('#matSpan14')
+const matSpan15 = document.querySelector('#matSpan15')
+const matSpan16 = document.querySelector('#matSpan16')
+const matSpan17 = document.querySelector('#matSpan17')
+const matSpan18 = document.querySelector('#matSpan18')
+const matSpan19 = document.querySelector('#matSpan19')
+
+
+
+
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    
+    let portion4 = document.querySelector('#portion4').value
+
+    //för att få ut siffran, den andra 
+//let mat2 = matSpan.innerHTML;
+const portion = portion4/4;
+
+
+
+// let resultat1 = matSpan1.innerHTML*portion;
+// let resultat2 = matSpan2.innerHTML*portion;
+// let resultat3 = matSpan3.innerHTML*portion;
+// let resultat4 = matSpan4.innerHTML*portion;
+// let resultat5 = matSpan5.innerHTML*portion;
+// let resultat6 = matSpan6.innerHTML*portion;
+// let resultat7 = matSpan7.innerHTML*portion;
+// let resultat8 = matSpan8.innerHTML*portion;
+// let resultat9 = matSpan9.innerHTML*portion;
+// let resultat10 = matSpan10.innerHTML*portion;
+// let resultat11 = matSpan11.innerHTML*portion;
+// let resultat12 = matSpan12.innerHTML*portion;
+// let resultat13 = matSpan13.innerHTML*portion;
+// let resultat14 = matSpan14.innerHTML*portion;
+// let resultat15 = matSpan15.innerHTML*portion;
+// let resultat16 = matSpan16.innerHTML*portion;
+// let resultat17 = matSpan17.innerHTML*portion;
+// let resultat18 = matSpan18.innerHTML*portion;
+// let resultat19 = matSpan19.innerHTML*portion;
+
+let resultat1 = 1*portion;
+let resultat2 = 4*portion;
+let resultat3 = 0.5*portion;
+let resultat4 = 0.5*portion;
+let resultat5 = 3*portion;
+let resultat6 = 2*portion;
+let resultat7 = 2*portion;
+let resultat8 = 2*portion;
+let resultat9 = 2*portion;
+let resultat10 = 2*portion;
+let resultat11 = 1*portion;
+let resultat12 = 0.5*portion;
+let resultat13 = 3*portion;
+let resultat14 = 2*portion;
+let resultat15 = 0.5*portion;
+let resultat16 = 1*portion;
+let resultat17 = 1*portion;
+let resultat18 = 1*portion;
+let resultat19 = 1*portion;
+
+matSpan1.innerHTML = resultat1;
+matSpan2.innerHTML = resultat2;
+matSpan3.innerHTML = resultat3;
+matSpan4.innerHTML = resultat4;
+matSpan5.innerHTML = resultat5;
+matSpan6.innerHTML = resultat6;
+matSpan7.innerHTML = resultat7;
+matSpan8.innerHTML = resultat8;
+matSpan9.innerHTML = resultat9;
+matSpan10.innerHTML = resultat10;
+matSpan11.innerHTML = resultat11;
+matSpan12.innerHTML = resultat12;
+matSpan13.innerHTML = resultat13;
+matSpan14.innerHTML = resultat14;
+matSpan15.innerHTML = resultat15;
+matSpan16.innerHTML = resultat16;
+matSpan17.innerHTML = resultat17;
+matSpan18.innerHTML = resultat18;
+matSpan19.innerHTML = resultat19;
+    
+})
